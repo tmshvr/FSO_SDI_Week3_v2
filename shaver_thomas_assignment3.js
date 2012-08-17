17d478ab61111ebaafc464f3a6938fe6c59fa5ac
Thomas Shaver <tmshvr@fullsail.edu>
2012-08-16 19:18:25 -0500

    Changed Coordinator arg to array, and changed the call to it; added announcement function; added mathematical decoration function.clear

--- shaver_thomas_assignment3.js
+++ shaver_thomas_assignment3.js
@@ -7,7 +7,7 @@
 
 // --- Global vars ? ---
 var teri = null,
-jsonText = { // --- Level 1: object ---
+jsonText = { // --- JSON Data & Level 1: object ---
     "menu": [ // --- Level 2: array ---
         { "food": "hamburgers", "have": false, "from": "Mike" }, // --- Level 3: object & Level 4: data ---
         { "food": "hotdogs", "have": false, "from": "Bobbi" },
@@ -29,24 +29,26 @@ jsonText = { // --- Level 1: object ---
     "total": 21
 },
 
-Coordinator = function( jsonObj ) { // --- Object argument ---
-    var menu = jsonObj.menu, // --- Property object ---
-        guestList = jsonObj.list, // --- Property array ---
-        totalGuests = jsonObj.total, // --- Property number ---
+Coordinator = function( jsonArray ) { // --- Array argument ---
+// --- Local vars ---
+    var menu = jsonArray[ 0 ], // --- Property object ---
+        guestList = jsonArray[ 1 ], // --- Property array ---
+        totalGuests = jsonArray[ 2 ], // --- Property number ---
         ready = false, // --- Property boolean ---
         tell = "Not yet, still people coming.", // --- Property string ---
         arrived = [],
         groupIterator = 0,
+        decorations = 0,
+        destroyed = 0,
 
-        getGroupAnnouncement = function( names ) { // --- String arg & Method Function ---
+        getGroupAnnouncement = function( names ) { // --- String arg & Method function ---
             // --- Mutator --- (Big one? Modifies arrived array.)
             var stringGroup = "", // for the else, and because Komodo calls this an anonymous function that doesn't always return a value, otherwise.
                 group = names.split( ", " );
                 announcement = " just got here.";
-
-            if( group.length === 1 ) { // One person.
+            if( group.length === 1 ) { // One person. --- Conditional ---
                 arrived.push( group[ 0 ]);
-                return ( group[ 0 ] + announcement );
+                return ( group[ 0 ] + announcement ); // --- Return string ---
             }
             else if( group.length === 2 ) { // Two people.
                 arrived.push( group[ 0 ]);
@@ -55,7 +57,7 @@ Coordinator = function( jsonObj ) { // --- Object argument ---
             }
             else { // Three or more people in the group.
                 for( var n = 0; n < group.length; n++ ) { // --- For loop ---
-                    // --- Nested Conditionals ---
+                    // --- Nested Conditional ---
                     if( n < ( group.length - 2 )) { // If it not the last person ", " concatenate them on.
                         stringGroup += group[ n ] + ", ";
                     }
@@ -68,56 +70,77 @@ Coordinator = function( jsonObj ) { // --- Object argument ---
                     arrived.push( group[ n ]);
                 };
             };
-            return stringGroup + announcement; // --- Return string ---
-        },
-        whoArrived = function() {
-            var arrival = getGroupAnnouncement( guestList[ groupIterator ]);
-            console.log( arrival );
-            groupIterator++;
+            return stringGroup + announcement;
         },
         whatArrived = function() { // --- Method procedure ---
             var foodArrived = "";
-            for( var k = 0; k < menu.length; k++ ) {
-                for( var j = 0; j < arrived.length; j++ ) {
-                    if( menu[ k ].from === arrived[ j ]) {
-                        if( menu[ k ].have === false ) { // --- Nested conditional ---
+                for( var k = 0; k < menu.length; k++ ) {
+                    for( var j = 0; j < arrived.length; j++ ) {
+                        if( menu[ k ].from === arrived[ j ] && menu[ k ].have === false ) { // --- Logical and ---
                             menu[ k ].have = true;
-                            foodArrived = "\t\t" + arrived[ j ] + " brought the " + menu[ k ].food + ".\n";
+                            foodArrived = "\t" + arrived[ j ] + " brought the " + menu[ k ].food + ".\n";
                         };
                     };
                 };
-            };
-            if( foodArrived != "" ) {
-                console.log( foodArrived );
-            };
+            return foodArrived;
         }, // --- Back ---
-        getGuestsLeft = function( alreadyHere ) {
-            return( totalGuests - alreadyHere );
+        getGuestsLeft = function( alreadyHere ) { // --- Number argument ---
+            return( totalGuests - alreadyHere ); // --- Return number ---
         },
-        getReady = function() { // --- Accessor (getter) ---
-            return ready;
+        getReady = function() { // --- Method Accessor (getter) ---
+            return ready; // --- Return boolean ---
         },
-        setReady = function( boolVal ) { // --- Boolean argument & Mutator (setter) ---
+        setReady = function( boolVal ) { // --- Boolean argument & Method Mutator (setter) ---
             if( boolVal === true || boolVal === false ) { // --- Logical OR ---
                 ready = boolVal;
             };
         },
+        announce = function( what ) { // --- String argument ---
+            console.log( "Announcement:\t" + what ); // --- Output ---
+        },
+        decorate = function( up, down ) {
+            decorations += up;
+            destroyed += down;
+        },
         timePasses = function( readyOrNot ) {
-            whoArrived();
-            whatArrived();
-            if( arrived.length - 1 === totalGuests ) {
-                setReady( true );
-            };
+            var arrival = getGroupAnnouncement( guestList[ groupIterator ]),
+                food = null
+            ;
+                announce( arrival );
+                groupIterator++;
+                food = whatArrived();
+                announce( food );
+                if( arrived.length - 1 === totalGuests ) {
+                    setReady( true );
+                };
+                if( ! arrived.length % 2 === 0 || arrived.length % 5 === 0 ) { // --- Logical or & Math ---
+                    console.log( "Put up more decorations." );
+                    if( arrived.length % 9 === 0 ) {
+                        console.log( "The kids's destroyed a decoration." );
+                        decorate( 2, 1 );
+                    }
+                    else {
+                        decorate( 2, 0 );
+                    };
+                };
         },
         throwParty = function() {
-            while( arrived.length < totalGuests ) {
+            while( arrived.length < totalGuests ) { // --- While loop ---
                  timePasses( getReady());
             };
+            console.log( "We got " + decorations + " decorations up, with " + destroyed + " decoration destroyed by the kids." );
+            announce( "Everybody's here, so I'm gonna call Jim and tell him we're ready - nobody leave." );
         };
-    return {
+    return { // --- Return object ---
         "throwParty": throwParty
     };
 };
 
-teri = new Coordinator( jsonText );
+var init = function( jsonObj ) { // --- Object argument ---
+    return [ jsonObj.menu, jsonObj.list, jsonObj.total ]; // --- Return array ---
+};
+
+// --- Start ---
+teri = new Coordinator(init( jsonText ));
 teri.throwParty();
+// --- Finish ---
